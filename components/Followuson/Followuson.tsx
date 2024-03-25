b574aed73c3404589da0b4b633a76c1f5d4b1144
Oyiga victor <victoroyiga01@gmail.com>
2024-03-25 09:22:06 +0100

    image silder

--- components/Followuson/Followuson.tsx
+++ components/Followuson/Followuson.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import ImageSlider  from '@/components/ImageSlider/ImageSlider'
 
 const Followuson = () => {
     return (
         <div className='w-[100%] h-[200px] '>
-            <div  className='ml-24'>
-               <h3 className='font-bold mb-2'>Instagram</h3>
-               <p>
-                 Follow us on @qodeinteractive
-               </p> 
+            <div className='ml-24'>
+                <h3 className='font-bold mb-2'>Instagram</h3>
+                <p>
+                    Follow us on @qodeinteractive
+                </p>
             </div>
+            <ImageSlider />
         </div>
     )
 }
--- components/ImageSlider/ImageSlider.tsx
+++ components/ImageSlider/ImageSlider.tsx
@@ -1,13 +1,27 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image';
+import styles from '@/components/ImageSlider/ImageSlider.module.css';
 
 const ImageSlider = () => {
 
-    const images = ['assets/video-clients-image-6.png','assets/Main-product-list-6.jpg', 'assets/instagram-img-3']
+  const images = [  'assets/instagram-img-3.jpg', 'assets/instagram-img-1.jpg', 'assets/instagram-img-4.jpg', 'assets/instagram-img-5.jpg','assets/instagram-img-6.jpg', 'assets/instagram-img-7.jpg', ]
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   return (
-    <div>
-      
+    <div className='w-full overflow-hidden mb-11 mt-12'>
+      <div className={`flex gap-36 justify-center mt-5 ${styles.slider}`}>
+        {images.map((image, index) => (
+          <Image className='size-40' key={index} src={`/${image}`} alt={`Image ${index + 1}`} width={100} height={100} />
+        ))}
+      </div>
     </div>
   )
 }
